eaaaffcb35cfff1d97420f10d9d70a3600e94c5f
Thanat0s <Thanspam@trollprod.org>
2015-01-25 20:26:16 +0100

    decrottePE with Xor swicth

--- decrottePE.py
+++ decrottePE.py
@@ -23,6 +23,7 @@ def decrotte(filearray, text):
   i=0
   dumped = False
   sys.stdout.flush()
+  sys.stdout.write( "\r"+int(columns)*" "+"\r")
   for M in range(0,len(filearray)-1):
     if int(filearray[M]) == ord('M') and  int(filearray[M+1]) == ord('Z') :
       i=(i+1) % 4
@@ -40,7 +41,7 @@ def decrotte(filearray, text):
         print ('\n[*] Found PE_%d.exe %d bytes saved\n' % (M,Z)),
       except:
         pass
-  sys.stdout.write( "\r"+int(columns)*" "+"\r")
+  #sys.stdout.write( "\r"+int(columns)*" "+"\r")
   return dumped
 
 
@@ -54,23 +55,36 @@ def xor(key,inc, filearray):
 
 rows, columns = os.popen('stty size', 'r').read().split()
 # Needs two arg if not... help
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
         print ('Extract PE from Dump')
-        print 'To Use: '+ sys.argv[0]+  ' filename'
+        print 'To Use: '+ sys.argv[0]+  ' filename [-xor]'
+        print '         [-xor] enable XOR Bruteforcing'
         sys.exit(1)
 FILENAME = sys.argv[1]
+
+XOR = False
+# Xor or not ?
+if len(sys.argv) == 3:
+  if sys.argv[2].upper() == "-XOR":
+    print "[i] Ok I Will Brutalizing the XorCipher"
+    XOR = True
 
 # Test if file exists
 if not os.path.isfile(FILENAME):
     print 'File not found'
     sys.exit(1)
-print "[*] Scanning %s" % FILENAME
+print "[i] Scanning %s" % FILENAME
 with open(FILENAME, 'rb') as f:
   filearray = bytearray(f.read())
 
-result = decrotte(filearray,"Simple search")
+result = decrotte(filearray,"Searching for PE")
 if result:
+  print"\n[i] I was victorious..."
   sys.exit(0)
+if XOR == False:
+  print "[!] Sorry nothing.. try with -XOR"
+  sys.exit(0)
+
 
 print("[!] No PE found, will try the chinese style; SIMPLE XOR")
 key = 1
@@ -91,3 +105,17 @@ while key < 256:
     sys.exit(0)
   key = key+1
 
+print("[!] No PE found, will try the chinese style; XOR Shifted looking every offsets")
+key = 1
+while key < 256:
+  offset = 0
+  while offset < 256:
+    key2 = (key + offset) % 256
+    result = decrotte(xor(key2,1,filearray),("8Bits Xor Inc key %d offset  %d"% (key,offset)))
+    if result:
+      sys.exit(0)
+    result = decrotte(xor(key2,-1,filearray),("8Bits Xor Dec key %d offset %d"% (key,offset)))
+    if result:
+      sys.exit(0)
+    offset = offset + 1
+  key=key+1
