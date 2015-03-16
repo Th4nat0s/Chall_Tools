07c85419a76c6d98e87775da3aa4a25f266e91ab
Thanat0s <Thanspam@trollprod.org>
2015-03-17 00:43:51 +0100

    Bugfix

--- machouillemacro.py
+++ machouillemacro.py
@@ -2,8 +2,6 @@
 # coding=utf-8   
 import re, sys, random, string
 
-
-
 #Dim vaLFVutQ As Integer^M
 #vaLFVutQ = 3^M
 #Do While vaLFVutQ < 32^M
@@ -21,23 +19,29 @@ def getparam(count):
 
 # Main Code #####
 def decoy():
-  N = 8 
+  N = random.randrange(16)
   param = getparam(1)
-  NAME = ''.join(random.choice(string.letters + string.digits) for _ in range(N))
-  NUM = random.randrange(64)
+  NAME = random.choice(string.letters) + ''.join(random.choice(string.letters + string.digits) for _ in range(N))
+  NUM = random.randrange(16)
   body = ("\nDim %s As Integer\n" % NAME)
   body += ("%s = %d\n" % (NAME, NUM))
-  body += ("Do While %s < %d\n" % (NAME, NUM + random.randrange(64)))
+  body += ("Do While %s < %d\n" % (NAME, NUM + 1 + random.randrange(15)))
   body += ("DoEvents: %s = %s + 1\n" % (NAME,NAME))
   body += ("Loop\n\n")
   return body
 
 def main():
+  PENDOWN = False
   mfile = getparam(1)
   f = open(mfile, 'rb')
   for line in f.readlines():
     print line
-    print decoy()
+    if line.upper().startswith("SUB"):
+      PENDOWN = True
+    if line.upper().startswith("END SUB"):
+      PENDOWN = False
+    if PENDOWN:
+      print decoy()
 
 
 
